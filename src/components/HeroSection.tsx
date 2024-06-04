474e0d8378c5e3f73466c6fc10fc76c25e77207e
tanvirtbf <tanver018765@gmail.com>
2024-06-04 15:46:51 +0600

    try moving border implement alone

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -6,7 +6,7 @@ function HeroSection() {
     <div className="h-auto md:h-[40rem] w-full rounded-md flex flex-col items-center justify-center relative overflow-hidden mx-auto py-10 md:py-0">
       <Spotlight
         className="-top-40 left-0 md:left-60 md:-top-20"
-        fill="white"
+        fill="red"
       />
       <div className="p-4 relative z-10 w-full text-center">
         <h1 className="mt-20 md:mt-0 text-4xl md:text-7xl font-bold bg-clip-text text-transparent bg-gradient-to-b from-neutral-50 to-neutral-400">
@@ -20,6 +20,7 @@ function HeroSection() {
         <div className="mt-4">
           <Link href={"/project"}>Explore Project</Link>
         </div>
+    
       </div>
     </div>
   );
