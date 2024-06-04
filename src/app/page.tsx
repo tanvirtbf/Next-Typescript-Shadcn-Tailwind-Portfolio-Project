433ca0e9b4ba6e75d970da8aea0056ed72e536e5
tanvirtbf <tanver018765@gmail.com>
2024-06-04 15:01:33 +0600

    hero section start

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,8 @@
-import Image from "next/image";
 
 export default function Home() {
   return (
-    <>
-      
-    </>
+    <main className="min-h-screen bg-black/[0.96] antialiased bg-grid-white/[0.02]">
+      <h1 className="text-2xl text-center">Hello World</h1>
+    </main>
   );
 }
